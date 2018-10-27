227bd98749c6f816d677a8c73c81bc07d7c00e0e
Russell <russellpenn123@gmail.com>
2018-10-27 19:34:37 +0100

    Can add and delete items from a list

--- Study Buddy 4.0/FirstViewController.swift
+++ Study Buddy 4.0/FirstViewController.swift
@@ -8,8 +8,45 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var txtAdd: UITextField!
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var btnAdd: UIButton!
+    
+    var list = ["Llama", "Alpaca", "Camel", "dat boi"]
+    
+    
+    @IBAction func addItem(_ sender: Any) {
+        if (txtAdd.text != ""){
+            list.append(txtAdd.text!)
+            txtAdd.text = ""
+            myTableView.reloadData()
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+    
 
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return (cell)
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            self.list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
