dbfe9ae0223b827164a5dc80431882ce3aab6784
Russell <russellpenn123@gmail.com>
2018-10-27 17:37:12 +0100

    Initial Commit

---
+++ Study Buddy 4.0/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Study Buddy 4.0
+//
+//  Created by Russell Penn on 27/10/2018.
+//  Copyright © 2018 HackSheffield Team. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Study Buddy 4.0/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Study Buddy 4.0
+//
+//  Created by Russell Penn on 27/10/2018.
+//  Copyright © 2018 HackSheffield Team. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
